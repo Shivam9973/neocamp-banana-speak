017d563ec384d4391b461e537cc4f01d43fd1577
Shivam9973 <62198777+Shivam9973@users.noreply.github.com>
2021-09-30 17:04:00 +0530

    Code for my work in js

---
+++ app.js
@@ -0,0 +1,25 @@
+let textArea = document.querySelector("textarea");
+let transButton = document.querySelector("#tButton");
+let outputText = document.querySelector("#output");
+
+let apiUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getFetchUrl(input) {
+    return apiUrl + "?text=" + input
+}
+
+function errorHandler(error) {
+    console.log("Error has Occured ! : " + error);
+    alert("Something went wrong with server! Please try again later");
+}
+
+transButton.addEventListener("click", function clickEventHandler() {
+    let inputText = textArea.value
+        // fetching API
+    fetch(getFetchUrl(inputText))
+        .then(Response => Response.json())
+        .then(json => {
+            let finaloutput = json.contents.translated;
+            outputText.innerText = finaloutput;
+        }).catch(errorHandler)
+});
